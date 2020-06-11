db1c63efe4840b4fcfe421234de8c9f80d9c2b40
Mudit Sharma <muditsharma23702@gmail.com>
2020-06-11 11:21:05 +0530

    Add files via upload

---
+++ HackerRankProblems/ArraysDS.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n, temp;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &a[i]);
+    }
+    for (int i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
